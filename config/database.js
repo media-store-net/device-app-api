904c19181852cb6acd4c761371526dc3660b3c86
Artur Voll <artur.voll@gmail.com>
2021-02-10 12:33:17 +0100

    chnaged tp mongoDB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/*module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -11,20 +11,24 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
+  },*/
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        client: 'mongo',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE'),
+        ssl: env('DATABASE_SSL', false),
+      },
+    },
   },
-  // defaultConnection: 'default',
-  // connections: {
-  //   default: {
-  //     connector: 'bookshelf',
-  //     settings: {
-  //       client: 'mysql',
-  //       host: env('DATABASE_HOST', 'localhost'),
-  //       port: env.int('DATABASE_PORT', 3306),
-  //       database: env('DATABASE_NAME', 'strapi'),
-  //       username: env('DATABASE_USERNAME', 'strapi'),
-  //       password: env('DATABASE_PASSWORD', 'strapi'),
-  //     },
-  //     options: {},
-  //   },
-  // },
 });
