45d8fdc2e729b5e63076111a3de5fb07cf3cf6d2
Artur Voll <artur.voll@gmail.com>
2021-02-12 13:01:36 +0100

    update: find devices by customer

--- api/devices/controllers/devices.js
+++ api/devices/controllers/devices.js
@@ -6,19 +6,25 @@
  */
 
 module.exports = {
-    customer_login: async (ctx) => {
-        const body = await ctx.request.body;
-        console.log(body);
-        const device = await strapi.query('Devices').findOne({
-            sn: body.sn,
-            pass: body.pass
-        });
+  customer_login: async (ctx) => {
+    const body = await ctx.request.body;
+    console.log(body);
+    const customer = await strapi.query('Companies').findOne({
+      kdnr: body.sn,
+      pass: body.pass
+    });
+    const device = await strapi.query('Devices').findOne({
+      sn: body.sn,
+      pass: body.pass
+    });
 
-        if (device && device.sn === body.sn && device.pass === body.pass) {
-            ctx.send(device)
-        }
-        else {
-            ctx.unauthorized(`You're not logged in!`);
-        }
-    },
+    if (customer && customer.kdnr === body.sn && customer.pass === body.pass) {
+      const devices = await strapi.query('Devices').find({'companie.kdnr': customer.kdnr});
+      ctx.send(devices);
+    } else if (device && device.sn === body.sn && device.pass === body.pass) {
+      ctx.send(device)
+    } else {
+      ctx.unauthorized(`Keine Übereinstimmung gefunden. Bitte Prüfe die Zugangsdaten!`);
+    }
+  },
 };
