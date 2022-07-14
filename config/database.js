9e998b93b7fcc2c7d56ad2898f2126eb52c83cfa
Artur Voll <artur.voll@gmail.com>
2022-07-14 15:04:05 +0200

    initial change to strapi v4 without data and api model

---
+++ config/database.js
@@ -0,0 +1,29 @@
+/*module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },*/
+module.exports = ({env}) => ({
+
+  connection: {
+    client: env("DATABASE_CLIENT", "mysql"),
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: false
+    },
+    debug: false,
+  },
+});
