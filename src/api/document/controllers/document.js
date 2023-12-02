50a14fa7d5998d958e28a570ad9c233fecb7fa0a
Artur Voll <artur.voll@gmail.com>
2023-12-02 11:22:02 +0100

    - document content type created and generate auto files to a given sn of device

--- src/api/document/controllers/document.js
+++ src/api/document/controllers/document.js
@@ -4,12 +4,27 @@
  * document controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::document.document', ({strapi})=> ({
+module.exports = createCoreController('api::document.document', ({strapi}) => ({
   async createPdf(ctx) {
-    const {sn, pass, docs} = ctx.request.body;
-    console.log({sn, pass, docs})
-    await strapi.service('api::document.document').confirmity({sn});
+    const {sn, pass, docs, locale} = ctx.request.body.data;
+
+    if (docs.length) {
+      const res = await Promise.all(docs.map(async (doc) => {
+        try {
+          return await strapi.service('api::document.document')[doc]({sn, pass, locale});
+        } catch (e) {
+          console.log(e)
+          return e
+        }
+      }))
+
+      const send = await strapi.service('api::document.document')
+        .prepareMediasAndSendEmail(res, sn)
+
+      return res;
+    }
+
   }
 }));
--- src/api/document/services/document.js
+++ src/api/document/services/document.js
@@ -9,10 +9,14 @@ const {createCoreService} = require('@strapi/strapi').factories;
 const fs = require('fs');
 const path = require('path');
 const PdfPrinter = require('pdfmake');
+const os = require("os");
+const i18next = require("i18next");
+const i18nBackend = require("i18next-http-backend");
 
 // Pfad zum öffentlichen Verzeichnis
 const publicPath = path.resolve(__dirname, '../../../../public');
-
+const en = require(path.join(publicPath, 'locales/en/en.json'));
+const de = require(path.join(publicPath, 'locales/de/de.json'));
 
 var fonts = {
   Roboto: {
@@ -23,11 +27,26 @@ var fonts = {
   }
 }
 
+i18next.use(i18nBackend).init({
+  fallbackLng: 'de',
+  resources: {
+    de, en
+  }
+});
+
+
 module.exports = createCoreService('api::document.document', ({strapi}) => ({
 
-  qrcode: async ({sn, pass}) => {
+  /**
+   *
+   * @param sn
+   * @param pass
+   * @param locale
+   * @return {Promise<Utils.Expression.If<Common.AreSchemaRegistriesExtended, GetValues<string, Utils.Guard.Never<ExtractFields<string, {populate: string[]}["fields"]>, Attribute.GetNonPopulatableKeys<string>>, ExtractPopulate<string, string[]>>, AnyEntity>>}
+   */
+  qrcode: async ({sn, pass, locale}) => {
     const doc = await strapi.db.query('api::document.document').findOne({
-      where: {slug: 'qrcode'},
+      where: {slug: 'qrcode', locale},
       populate: ['header_image.url', 'disclosure_image.url', 'footer_image.url']
     })
 
@@ -35,7 +54,7 @@ module.exports = createCoreService('api::document.document', ({strapi}) => ({
       where: {sn}
     })
 
-    const filename = `Passwort und QR-Code_${device.sn.toUpperCase()}.pdf`;
+    const filename = `${i18next.t('qrcode.filename', {lng: locale})}_${device.sn.toUpperCase()}.pdf`;
     const appLogin = `https://device-api.powasert.de/customer-login?sn=${device.sn.toUpperCase()}`;
     const printer = new PdfPrinter(fonts);
 
@@ -72,7 +91,7 @@ module.exports = createCoreService('api::document.document', ({strapi}) => ({
             style: 'text'
           },
           {
-            text: 'Tauchen Schwierigkeiten auf?',
+            text: i18next.t('qrcode.difficult', {lng: locale}),
             style: 'bolderText',
             alignment: 'center'
           },
@@ -83,7 +102,7 @@ module.exports = createCoreService('api::document.document', ({strapi}) => ({
             style: 'text'
           },
           {
-            text: 'Kein Grund zur Panik – Fragen Sie uns!',
+            text: i18next.t('qrcode.panic', {lng: locale}),
             style: 'bolderText',
             alignment: 'center'
           },
@@ -124,26 +143,71 @@ module.exports = createCoreService('api::document.document', ({strapi}) => ({
         }
       })
       pdfDoc.pipe(fs.createWriteStream(path.join(publicPath, 'tmp', filename)));
-      pdfDoc.end();
+      const pdfEnd = await pdfDoc.end();
 
-      //TODO upload and set as entity to device
+      // on end event upload the file
+      return strapi.service('api::document.document')
+        .checkFileExist(path.join(publicPath, 'tmp', filename)).then(async () => {
+          // prepare the file with meta
+          const file = await strapi.service('api::document.document')
+            .readAndPrepareFile(path.join(publicPath, 'tmp', filename), filename)
+
+          // upload the file
+          const upload = await strapi.service('api::document.document')
+            .saveFileToDatabase({data: {}, files: [file]})
+
+          // find the doctype
+          const doctype = await strapi.db.query('api::doctype.doctype').findOne(
+            {where: {title: 'Dokumentation'}}
+          )
+
+          // create media and add to device
+          const media = await strapi.entityService.create('api::media.media', {
+            data: {
+              filename: upload[0].name,
+              url: upload[0].id,
+              doctype: doctype.id,
+              devices: [device.id]
+            }
+          })
+
+          if (media.id) {
+            fs.rmSync(path.join(publicPath, 'tmp', filename))
+          }
+
+          return await strapi.entityService.findOne('api::media.media', media.id, {
+            populate: ['url']
+          })
+
+        })
+
     } catch (e) {
       console.log(e)
+      return e
     }
 
   },
-  confirmity: async ({sn}) => {
+
+  /**
+   *
+   * @param sn
+   * @param pass
+   * @param locale
+   * @return {Promise<Utils.Expression.If<Common.AreSchemaRegistriesExtended, GetValues<string, Utils.Guard.Never<ExtractFields<string, {populate: string[]}["fields"]>, Attribute.GetNonPopulatableKeys<string>>, ExtractPopulate<string, string[]>>, AnyEntity>>}
+   */
+  confirmity: async ({sn, pass, locale}) => {
     const doc = await strapi.db.query('api::document.document').findOne({
-      where: {slug: 'confirmity'},
+      where: {slug: 'confirmity', locale},
       populate: true
     })
 
     const device = await strapi.db.query('api::device.device').findOne({
       where: {sn},
+      locale,
       populate: true
     })
 
-    const filename = `Einbauerklärung ${device.sn.toUpperCase()}.pdf`;
+    const filename = `${i18next.t('confirmity.filename', {lng: locale})} ${device.sn.toUpperCase()}.pdf`;
     // const appLogin = `https://device-api.powasert.de/customer-login?sn=${device.sn.toUpperCase()}`;
     const printer = new PdfPrinter(fonts);
 
@@ -262,11 +326,348 @@ module.exports = createCoreService('api::document.document', ({strapi}) => ({
         }
       })
       pdfDoc.pipe(fs.createWriteStream(path.join(publicPath, 'tmp', filename)));
-      pdfDoc.end();
+      const pdfEnd = await pdfDoc.end();
 
-      //TODO upload and set as entity to device
+      // on end event upload the file
+      return strapi.service('api::document.document').checkFileExist(path.join(publicPath, 'tmp', filename)).then(async () => {
+        // prepare the file with meta
+        const file = await strapi.service('api::document.document')
+          .readAndPrepareFile(path.join(publicPath, 'tmp', filename), filename)
+
+        // upload the file
+        const upload = await strapi.service('api::document.document')
+          .saveFileToDatabase({data: {}, files: [file]})
+
+        // find the doctype
+        const doctype = await strapi.db.query('api::doctype.doctype').findOne(
+          {where: {title: 'Dokumentation'}}
+        )
+
+        // create media and add to device
+        const media = await strapi.entityService.create('api::media.media', {
+          data: {
+            filename: upload[0].name,
+            url: upload[0].id,
+            doctype: doctype.id,
+            devices: [device.id]
+          }
+        })
+
+        if (media.id) {
+          fs.rmSync(path.join(publicPath, 'tmp', filename))
+        }
+
+        return await strapi.entityService.findOne('api::media.media', media.id, {
+          populate: ['url']
+        })
+
+      })
     } catch (e) {
       console.log(e)
+      return e
     }
-  }
+  },
+
+  /**
+   *
+   * @param sn
+   * @param pass
+   * @param locale
+   * @return {Promise<Utils.Expression.If<Common.AreSchemaRegistriesExtended, GetValues<string, Utils.Guard.Never<ExtractFields<string, {populate: string[]}["fields"]>, Attribute.GetNonPopulatableKeys<string>>, ExtractPopulate<string, string[]>>, AnyEntity>>}
+   */
+  mounting: async ({sn, pass, locale}) => {
+    const doc = await strapi.db.query('api::document.document').findOne({
+      where: {slug: 'mounting', locale},
+      populate: true
+    })
+
+    const device = await strapi.db.query('api::device.device').findOne({
+      where: {sn},
+      populate: true
+    })
+
+    const filename = `${i18next.t('mounting.filename', {lng: locale})} ${device.sn.toUpperCase()}.pdf`;
+    // const appLogin = `https://device-api.powasert.de/customer-login?sn=${device.sn.toUpperCase()}`;
+    const printer = new PdfPrinter(fonts);
+
+    try {
+      const pdfDoc = printer.createPdfKitDocument({
+        pageSize: 'A4',
+        pageMargins: [40, 100, 40, 60],
+        header: {
+          image: path.join(publicPath, doc.header_image.url),
+          width: 500,
+          style: 'header'
+        },
+        content: [
+          {
+            text: doc.title,
+            style: 'title'
+          },
+          {
+            text: doc.subtitle,
+            style: 'text',
+            alignment: 'center'
+          },
+          {
+            text: doc.intro,
+            style: 'text',
+            alignment: 'left'
+          },
+          {
+            columns: [
+              {
+                text: 'POWASERT',
+                alignment: 'left',
+                margin: [140, 0, 0, 0]
+              },
+              {
+                text: device.part.desc,
+                alignment: 'left'
+              }
+            ]
+          },
+          {
+            columns: [
+              {
+                text: 'Typ:',
+                alignment: 'left',
+                margin: [140, 0, 0, 0]
+              },
+              {
+                text: device.part.title,
+                alignment: 'left'
+              }
+            ]
+          },
+          {
+            columns: [
+              {
+                text: 'Fabr. Nr.:',
+                alignment: 'left',
+                margin: [140, 0, 0, 0]
+              },
+              {
+                text: device.sn.toUpperCase(),
+                alignment: 'left'
+              }
+            ]
+          },
+          {
+            text: doc.content,
+            style: 'text',
+            alignment: 'left',
+            margin: [0, 20, 0, 0]
+          },
+          {
+            image: path.join(publicPath, doc.disclosure_image.url),
+            alignment: 'left',
+            width: 150,
+            margin: [0, 20, 0, 0]
+          }
+        ],
+        footer: {
+          image: path.join(publicPath, doc.footer_image.url),
+          alignment: 'center',
+          width: 500,
+          style: 'footer'
+        },
+        styles: {
+          header: {
+            marginTop: 20,
+            alignment: 'center'
+          },
+          title: {
+            marginTop: 60,
+            alignment: 'center',
+            fontSize: 16,
+            bold: true,
+            italics: true,
+            marginBottom: 15
+          },
+          text: {
+            marginBottom: 10,
+            alignment: 'center'
+          },
+          qrcode: {
+            marginTop: 10,
+            marginBottom: 10
+          },
+          bolderText: {
+            bold: true,
+            marginBottom: 10
+          },
+          footer: {
+            marginBottom: 80
+          }
+        },
+        defaultStyle: {
+          fontSize: 12
+        }
+      })
+      pdfDoc.pipe(fs.createWriteStream(path.join(publicPath, 'tmp', filename)));
+      const pdfEnd = await pdfDoc.end();
+
+      // on end event upload the file
+      return strapi.service('api::document.document').checkFileExist(path.join(publicPath, 'tmp', filename)).then(async () => {
+        // prepare the file with meta
+        const file = await strapi.service('api::document.document')
+          .readAndPrepareFile(path.join(publicPath, 'tmp', filename), filename)
+
+        // upload the file
+        const upload = await strapi.service('api::document.document')
+          .saveFileToDatabase({data: {}, files: [file]})
+
+        // find the doctype
+        const doctype = await strapi.db.query('api::doctype.doctype').findOne(
+          {where: {title: 'Dokumentation'}}
+        )
+
+        // create media and add to device
+        const media = await strapi.entityService.create('api::media.media', {
+          data: {
+            filename: upload[0].name,
+            url: upload[0].id,
+            doctype: doctype.id,
+            devices: [device.id]
+          }
+        })
+
+        if (media.id) {
+          fs.rmSync(path.join(publicPath, 'tmp', filename))
+        }
+
+        return await strapi.entityService.findOne('api::media.media', media.id, {
+          populate: ['url']
+        })
+
+      })
+
+    } catch (e) {
+      console.log(e)
+      return e
+    }
+  },
+
+  /**
+   * Service-Funktion zum Auslesen der Datei aus einem Pfad
+   *
+   * @param string filePath
+   * @param string  fileName
+   * @return {Promise<unknown>}
+   */
+  readAndPrepareFile: (filePath, fileName) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const fileStats = fs.statSync(filePath);
+          const fileSizeInBytes = fileStats.size;
+
+          resolve({
+            data: Buffer.from(data), // Die Dateidaten als Buffer
+            path: filePath,
+            type: 'application/pdf', // Der Dateityp (hier als Beispiel 'application/octet-stream')
+            name: fileName, // Der Dateiname (hier als Beispiel 'example.jpg')
+            size: fileSizeInBytes
+          });
+        }
+      });
+    });
+  },
+
+  /**
+   * Service-Funktion zum Speichern einer Datei aus dem Upload-Plugin in die Datenbank
+   * @param {Object} data
+   * @param {Object} file - Die hochgeladene Datei
+   * @param {Object} options
+   */
+  async saveFileToDatabase(obj) {
+    // Importiere das Upload-Plugin
+    const uploadPlugin = strapi.plugins['upload'].services.upload;
+    const {getAPIUploadFolder} = strapi.plugins['upload'].services['api-upload-folder'];
+    const apiUploadFolder = await getAPIUploadFolder();
+
+    if (Array.isArray(obj.files)) {
+      obj.data.fileInfo = obj.data.fileInfo || [];
+      obj.data.fileInfo = obj.files.map((_f, i) => ({...obj.data.fileInfo[i], folder: apiUploadFolder.id}));
+    } else {
+      obj.data.fileInfo = {...obj.data.fileInfo, folder: apiUploadFolder.id};
+    }
+
+    //console.log(apiUploadFolder)
+
+    try {
+      // Speichere die Datei im Upload-Plugin und erhalte die Dateiinformationen
+      const uploadedFile = await uploadPlugin.upload(obj)
+
+      return uploadedFile;
+    } catch (error) {
+      // Handle Fehler hier
+      console.error('Fehler beim Speichern der Datei:', error);
+      return error;
+    }
+  },
+
+  /**
+   * Service-Funktion zum prüfen, ob die Datei existiert und lesbar ist
+   *
+   * @param path
+   * @param timeout
+   * @return {Promise<unknown>}
+   */
+  async checkFileExist(path, timeout = 2000) {
+    let totalTime = 0;
+    let checkTime = timeout / 10;
+
+    return await new Promise((resolve, reject) => {
+      const timer = setInterval(function () {
+
+        totalTime += checkTime;
+
+        let fileExists = fs.existsSync(path);
+
+        if (fileExists || totalTime >= timeout) {
+          clearInterval(timer);
+
+          resolve(fileExists);
+
+        }
+      }, checkTime);
+    });
+  },
+
+  /**
+ * Diese Funktion bereitet die Medien vor und sendet eine E-Mail.
+ * @param {Array} medias - Eine Liste von Medien.
+ * @param {string} sn - Die Seriennummer des Geräts.
+ */
+async prepareMediasAndSendEmail(medias = [], sn = '') {
+    if (medias.length) {
+      // Geräte SN im Subject einfügen
+      const attachments = await Promise.all(medias.map(async (media) => {
+        const filePath = path.join(publicPath, media.url.url);
+        const fileCopy = path.join(publicPath, 'tmp', media.filename);
+        fs.copyFileSync(filePath, fileCopy);
+
+        const file = await strapi.service('api::document.document').readAndPrepareFile(fileCopy, media.filename)
+
+        return file
+      }))
+
+      const res = await strapi.plugins['email'].services.email.send({
+        to: 'info@pcservice-voll.de',
+        from: 'noreply@media-store.net',
+        subject: `Dateien für SN: ${sn} wurden generiert!`,
+        html: `<p>Anbei die neuen Dateien</p>`,
+        attachments: attachments
+      }).then(() => {}).catch((err) => err);
+
+      for (const file of fs.readdirSync(path.join(publicPath, 'tmp'))) {
+        if (file !== '.gitkeep') fs.unlinkSync(path.join(publicPath, 'tmp', file));
+      }
+
+    }
+}
 }));
